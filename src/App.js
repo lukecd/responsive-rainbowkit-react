aa67b8d098ef3c7eeadc9b53defc2c63348cb9dc
The Luke <luke@SPStories.com>
2022-06-12 12:31:48 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import Layout from "./components/Layout";
+import { Switch, Route } from "react-router-dom";
+import {Helmet} from "react-helmet";
+import Home from "./pages/Home";
+import PageOne from "./pages/PageOne";
+import PageTwo from "./pages/PageTwo";
+import PageThree from "./pages/PageThree";
+import PageFour from "./pages/PageFour";
+import PageFive from "./pages/PageFive";
+import PageSix from "./pages/PageSix";
+import RainbowFavicon from "./assets/images/rainbow-favicon-16x16.png";
+
+function App() {
+    return (
+        <>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>roy g biv</title>
+            <link rel="icon" type="image/png" href="{RainbowFavicon}" sizes="16x16" />
+        </Helmet>
+        <Layout>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/page-one">
+                    <PageOne />
+                </Route>
+                <Route path="/page-two">
+                    <PageTwo />
+                </Route>
+                <Route path="/page-three">
+                    <PageThree />
+                </Route>
+                <Route path="/page-four">
+                    <PageFour />
+                </Route>
+                <Route path="/page-five">
+                    <PageFive />
+                </Route>
+                <Route path="/page-six">
+                    <PageSix />
+                </Route>
+            </Switch>
+        </Layout>
+        </>
+    );
+}
+
+export default App;
---
+++ src/pages/PageTwo.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const PageTwo = () => {
+    return (
+        <>
+        <div className="container orange">
+            <header className="header">
+                <h1>page title</h1>
+            </header>
+            <article className="main">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </article>
+            <aside className="aside aside1">
+                <h1>aside 1</h1>
+            </aside>
+            <aside className="aside aside2">
+                <h1>aside 2</h1>
+            </aside> 
+            <footer className="footer">
+                <h1>footer</h1>
+            </footer>
+        </div>
+        </>
+    );
+};
+
+export default PageTwo;
